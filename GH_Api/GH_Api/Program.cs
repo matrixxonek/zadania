6f207051c3b470683fea8d843bc4fa07f4f98fab
matrixxonek <matrixxon@gmail.com>
2022-10-03 10:58:06 +0200

    Aplikacja do repo(ogarnąłem, działa)

---
+++ GH_Api/GH_Api/Program.cs
@@ -0,0 +1,20 @@
+﻿using Octokit;
+using System.Linq;
+
+var client = new GitHubClient(new ProductHeaderValue("my-app"));
+Console.WriteLine("Podaj Personal Access Token");
+var token = Console.ReadLine();
+var tokenAuth = new Credentials(token);
+client.Credentials = tokenAuth;
+var userInfo = await client.User.Current();
+Console.WriteLine(userInfo.Login + userInfo.PublicRepos);
+List<Repository> GetGithubRepository()
+{
+    return client.Repository.GetAllForCurrent().Result.ToList();
+}
+List<Repository> repos = GetGithubRepository();
+foreach (var repo in repos)
+    Console.WriteLine(repo.Name);
+
+
+
