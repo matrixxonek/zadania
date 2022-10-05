2b600e7079b0ddfb89ab6f6259e10a9b3ce8d194
matrixxonek <matrixxon@gmail.com>
2022-10-05 14:19:21 +0200

    pobieranie issues, porownanie ich wybranie roznic ustawienie intervalu

---
+++ Issues/Issues/LoginData.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.Metrics;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Text;
+using System.Threading.Tasks;
+using Octokit;
+
+public class LoginData{
+    public string token;
+    public string repoName;
+    public string[] loginData = new string[2];
+
+    public void getDataFromFile()
+    {
+        StreamReader reader = new StreamReader(@"C:\Users\Admin\source\repos\zadania\Issues\config.txt");
+        int i = 0;
+        foreach (string line in System.IO.File.ReadLines(@"C:\Users\Admin\source\repos\zadania\Issues\config.txt"))
+        {
+            loginData[i] = line;
+            i++;
+        }
+        token = loginData[0];
+        repoName = loginData[1];
+        Console.WriteLine(repoName);
+    }
+    public void getDataFromUser()
+    {
+        Console.WriteLine("Podaj token");
+        token = Console.ReadLine();
+        Console.WriteLine("Nazwe repo");
+        repoName = Console.ReadLine();
+    }
+    public LoginData()
+    {
+        try
+        {
+            getDataFromFile();
+        }
+        catch(Exception ex)
+        {
+            Console.WriteLine(ex.Message);
+            getDataFromUser();
+        }
+    }
+}
---
+++ Issues/Issues/Program.cs
@@ -0,0 +1,56 @@
+﻿using Octokit;
+
+static class Program
+{
+    public static GitHubClient client = new GitHubClient(new Octokit.ProductHeaderValue("my-app"));
+    public static LoginData login = new LoginData();
+    public static Credentials tokenAuth = new Credentials(login.token);
+    public static string userName;
+    public static IReadOnlyList<Issue> newIssues;
+    public static IReadOnlyList<Issue> startIssues;
+    public static IReadOnlyList<Issue> differentIssues;
+    public static User user;
+    public static System.Timers.Timer timer;
+    private static System.Timers.Timer aTimer;
+    static public void Main(String[] args)
+    {
+        client.Credentials = tokenAuth;
+        user = client.User.Current().Result;
+        userName = user.Login;
+        startIssues = getIssues();
+        aTimer = new System.Timers.Timer();
+        setTime();
+    }
+    static public IReadOnlyList<Issue> getIssues()
+    {
+        return client.Issue.GetAllForRepository(userName, login.repoName).Result;
+    }
+    static public void setNewIssue()
+    {
+        newIssues = getIssues();
+    }
+    static public IReadOnlyList<Issue> compareIssue()
+    {
+        return newIssues.Except(startIssues).Union(startIssues.Except(newIssues)).ToList();
+    }
+    static public void setDifferentIssues()
+    {
+        differentIssues = compareIssue();
+        startIssues = newIssues;
+    }
+    static void setTime()
+    {
+        Console.WriteLine("Podaj czas w sekundach");
+        int i = int.Parse(Console.ReadLine());
+        aTimer.Interval = i * 1000;
+        aTimer.Elapsed += OnTimedEvent;
+        aTimer.AutoReset = true;
+        aTimer.Enabled = true;
+        Console.WriteLine("Press the Enter key to exit the program at any time... ");
+        Console.ReadLine();
+    }
+    private static void OnTimedEvent(Object source, System.Timers.ElapsedEventArgs e)
+    {
+        //sendMessage();
+    }
+}
