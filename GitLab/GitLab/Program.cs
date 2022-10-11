c9c85a0f4eb496f32351378a95ae253f5943d59a
matrixxonek <matrixxon@gmail.com>
2022-10-11 14:33:53 +0200

    GitLab zadanie

---
+++ GitLab/GitLab/Program.cs
@@ -0,0 +1,11 @@
+﻿using GitLabApiClient;
+using GitLabApiClient.Models.MergeRequests.Responses;
+
+class Program
+{
+    static public void Main(String[] args)
+    {
+        Login login = new Login();
+        
+    }
+}
---
+++ GitLab/GitLab/Requests.cs
@@ -0,0 +1,27 @@
+﻿using GitLabApiClient.Models.MergeRequests.Responses;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+class Requests
+{
+    public string id;
+    IList<MergeRequest> requests;
+    public void setRepo()
+    {
+        Console.WriteLine("Podaj id swojego repo");
+        id = Console.ReadLine();
+    }
+    public IList<MergeRequest> setMergeRequests()
+    {
+        return Login.client.MergeRequests.GetAsync(id).Result;
+    }
+    public void showMergeRequests()
+    {
+        requests = setMergeRequests();
+        foreach (MergeRequest request in requests)
+            Console.WriteLine(request.Title);
+    }
+}
